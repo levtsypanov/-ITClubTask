81e25cf520b7ea6c34157a2d7e5da8829185393e
levtsypanov <levdark7@gmail.com>
2020-07-08 03:21:45 +0300

    first commit

---
+++ precache-manifest.895a15596a18fba3c3bb6aca9d2b84ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f44166f7ed3aae14686d179c91315a4d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6f6545545ec75ab23b3",
+    "url": "/static/css/2.6bab1db4.chunk.css"
+  },
+  {
+    "revision": "5079044b7f0bc4796cb9",
+    "url": "/static/css/main.8b6086d1.chunk.css"
+  },
+  {
+    "revision": "b6f6545545ec75ab23b3",
+    "url": "/static/js/2.a1286d24.chunk.js"
+  },
+  {
+    "revision": "5079044b7f0bc4796cb9",
+    "url": "/static/js/main.1efd2f9a.chunk.js"
+  },
+  {
+    "revision": "b2e24dffd3713a085711",
+    "url": "/static/js/runtime-main.193a4bd0.js"
+  }
+]);
